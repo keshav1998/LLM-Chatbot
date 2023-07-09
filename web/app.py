7e10f52971af99fe228fe76557a62d26f284953f
Keshav Mishra <keshav.mishra0298@gmail.com>
2023-07-10 04:51:13 +0530

    add: cold start check

--- web/app.py
+++ web/app.py
@@ -5,15 +5,16 @@ import requests
 import json
 
 def get_response(input_text):
-    url = "http://" + "127.0.0.1" + ":" + "9098" + "/api/api/chat/"
+    # url from docker bridge network
+    url = "http://" + "llm-backend" + ":" + "8967" + "/api/api/chat/"
     payload = json.dumps({"question": input_text, "description":''})  # convert dict to json string
     headers = {'Content-Type': 'application/json'}
     response = requests.request("POST", url, headers=headers, data=payload)
-    return response.json()["content"]
+    if response.text == "":
+        return "No response from LLM API, wait for a few seconds and try again."
+    else:
+        return response.json()["content"]
 
 iface = gr.Interface(fn=get_response, inputs="textbox", outputs="textbox")
-iface.launch(share=True)
-
-# Run the app
-# python app.py
+iface.launch(server_name="0.0.0.0", server_port=7860)
 
